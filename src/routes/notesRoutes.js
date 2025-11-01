fd27df7df992a9e68356dd84b2ca60add6594136
AntonyWhatsup <antonkrykhta@gmail.com>
2025-11-01 17:02:19 +0100

    end2

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -14,18 +14,12 @@ import {
 } from '../validations/notesValidation.js';
 import { authenticate } from '../middleware/authenticate.js';
 
-
 const router = express.Router();
 
-
 router.get('/', authenticate, getAllNotes);
-
-router.get('/:id', authenticate, celebrate(noteIdSchema), getNoteById);
-
-router.post('/', authenticate, celebrate(createNoteSchema), createNote);
-
-router.patch('/:id', authenticate, celebrate(updateNoteSchema), updateNote);
-
-router.delete('/:id', authenticate, celebrate(noteIdSchema), deleteNote);
+router.get('/:id', authenticate, celebrate({ params: noteIdSchema }), getNoteById);
+router.post('/', authenticate, celebrate({ body: createNoteSchema }), createNote);
+router.patch('/:id', authenticate, celebrate({ params: noteIdSchema }), celebrate({ body: updateNoteSchema }), updateNote);
+router.delete('/:id', authenticate, celebrate({ params: noteIdSchema }), deleteNote);
 
 export default router;
